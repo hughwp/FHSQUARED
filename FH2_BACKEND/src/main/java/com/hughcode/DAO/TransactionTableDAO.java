6e28e08817f5ea5aeccdfd3adbfde3f9c09c57a1
Hugh <hughtaylor2003@icloud.com>
2026-08-24 14:30:26 +0100

    Added the functionality of both x amount in one day rule, but still waiting on francilies DAO so the amount is hardcoded in...

--- FH2_BACKEND/src/main/java/com/hughcode/DAO/TransactionTableDAO.java
+++ FH2_BACKEND/src/main/java/com/hughcode/DAO/TransactionTableDAO.java
@@ -27,8 +27,18 @@ public class TransactionTableDAO {
     public static void insertTransaction(Transaction transaction) throws SQLException {
         String query = "INSERT INTO transactions (transaction_id, account_id, payer_fname, payer_lname, payee_id, merchantName, amount, transaction_type, timestamp, status) " +
                 "VALUES ('" + transaction.transactionId + "', '" + transaction.accountId + "', '" + transaction.payerFname + "', '" + transaction.payerLname + "', '" + transaction.payeeId + "', '" + transaction.merchantName + "', " + transaction.amount + ", '" + transaction.transactionType + "', '" + transaction.timestamp + "', '" + transaction.status + "')";
-
         DatabaseConnection.getConnection().createStatement().executeUpdate(query);
+    }
+
+
+    public static double getTotalTransactionInLastDay(Transaction transaction) throws SQLException{
+        String query = "SELECT SUM(amount) as total FROM transactions WHERE account_id = '" + transaction.accountId + "' AND timestamp >= NOW() - INTERVAL '1 day'";
+        var resultSet = DatabaseConnection.getConnection().createStatement().executeQuery(query);
+
+        if (resultSet.next()) {
+            return resultSet.getDouble("total");
+        }
+        return 0.0;
     }
 
 }
