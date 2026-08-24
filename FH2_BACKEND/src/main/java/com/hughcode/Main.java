5243d93d63898e681a72a86b2af8ba27a509970f
Hugh <hughtaylor2003@icloud.com>
2026-08-24 15:43:25 +0100

    adding some stuff in main

--- FH2_BACKEND/src/main/java/com/hughcode/Main.java
+++ FH2_BACKEND/src/main/java/com/hughcode/Main.java
@@ -4,6 +4,7 @@ import java.util.concurrent.*;
 import com.hughcode.DAO.TransactionPublisherDAO;
 import com.hughcode.Transaction;
 import com.hughcode.DatabaseConnection;
+import com.hughcode.rules.TransactionAgainstRules;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
@@ -27,6 +28,8 @@ public class Main {
             try {
                 Transaction transaction = transactionQueue.take();
                 System.out.println("Received transaction: " + transaction.toString());
+                System.out.println("Processing  of the amount £"+transaction.amount);
+                System.out.println(TransactionAgainstRules.test(transaction));
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
                 break;
