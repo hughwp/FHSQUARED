cf9f8396d52884e28efc3cef2b706ce36561345d
kueendaka <haneen.elhag@outlook.com>
2026-08-25 15:12:55 +0000

    fixed datetimestamp endpoints

--- FH2_BACKEND/src/main/java/com/hughcode/Handlers/AllAlertHandler.java
+++ FH2_BACKEND/src/main/java/com/hughcode/Handlers/AllAlertHandler.java
@@ -1,18 +1,27 @@
 package com.hughcode.Handlers;
 
-import com.google.gson.Gson;
+import com.google.gson.*;
 import com.hughcode.Alert;
 import com.hughcode.DAO.AlertsDAO;
 import com.hughcode.SendResponse;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 public class AllAlertHandler implements HttpHandler {
 
     private final AlertsDAO alertsDAO = new AlertsDAO();
-    private final Gson gson = new Gson();
+
+    private static final DateTimeFormatter formatter =
+            DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSSSSS");
+
+    private static final Gson gson = new GsonBuilder()
+            .registerTypeAdapter(LocalDateTime.class, (JsonDeserializer<LocalDateTime>) (json, typeOfT, context)
+                    -> LocalDateTime.parse(json.getAsString(), formatter)) .registerTypeAdapter(LocalDateTime.class, (JsonSerializer<LocalDateTime>) (src, typeOfSrc, context)
+                    -> new JsonPrimitive(src.toString())) .create();;
 
     @Override
     public void handle(HttpExchange exchange) {
@@ -25,6 +34,7 @@ public class AllAlertHandler implements HttpHandler {
             }
         } catch (Exception e) {
             try {
+                System.out.println("Error handling request: " + e.toString());
                 SendResponse.sendResponse(exchange, 500, "{\"error\":\"Internal server error\"}");
             } catch (Exception ex) {
                 System.out.println("Error sending error response: " + ex.toString());
