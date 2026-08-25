ff630445bb045c72656e6fe9c99370cfb49bf0d6
Hugh <hughtaylor2003@icloud.com>
2026-08-25 11:08:16 +0100

    Added the boiler plate

---
+++ FH2_BACKEND/src/main/java/com/hughcode/Handlers/ChangeAlertStatus.java
@@ -0,0 +1,27 @@
+package com.hughcode.Handlers;
+
+import com.hughcode.SendResponse;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+public class ChangeAlertStatus implements HttpHandler {
+
+    public ChangeAlertStatus() {}
+
+    @Override
+    public void handle(HttpExchange exchange) {
+        try {
+            if ("GET".equals(exchange.getRequestMethod())) {
+                SendResponse.sendResponse(exchange, 200, "ChangeAlertStatus is up and running");
+            } else {
+                SendResponse.sendResponse(exchange, 405, "Method not allowed");
+            }
+        } catch (Exception e) {
+            try {
+                SendResponse.sendResponse(exchange, 500, "Internal server error");
+            } catch (Exception ex) {
+                System.out.println("Error sending error response: " + ex.toString());
+            }
+        }
+    }
+}
