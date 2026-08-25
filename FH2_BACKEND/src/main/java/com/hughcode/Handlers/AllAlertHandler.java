3ae30745a75deab57b0b6807380684cf123ecd7d
kueendaka <haneen.elhag@outlook.com>
2026-08-25 14:27:00 +0000

    implement all endpoints

--- FH2_BACKEND/src/main/java/com/hughcode/Handlers/AllAlertHandler.java
+++ FH2_BACKEND/src/main/java/com/hughcode/Handlers/AllAlertHandler.java
@@ -1,24 +1,31 @@
 package com.hughcode.Handlers;
 
+import com.google.gson.Gson;
+import com.hughcode.Alert;
+import com.hughcode.DAO.AlertsDAO;
 import com.hughcode.SendResponse;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
+import java.util.List;
+
 public class AllAlertHandler implements HttpHandler {
 
-    public AllAlertHandler() {}
+    private final AlertsDAO alertsDAO = new AlertsDAO();
+    private final Gson gson = new Gson();
 
     @Override
     public void handle(HttpExchange exchange) {
         try {
             if ("GET".equals(exchange.getRequestMethod())) {
-                SendResponse.sendResponse(exchange, 200, "AllAlertHandler is up and running");
+                List<Alert> alerts = alertsDAO.getAllAlerts();
+                SendResponse.sendResponse(exchange, 200, gson.toJson(alerts));
             } else {
-                SendResponse.sendResponse(exchange, 405, "Method not allowed");
+                SendResponse.sendResponse(exchange, 405, "{\"error\":\"Method not allowed\"}");
             }
         } catch (Exception e) {
             try {
-                SendResponse.sendResponse(exchange, 500, "Internal server error");
+                SendResponse.sendResponse(exchange, 500, "{\"error\":\"Internal server error\"}");
             } catch (Exception ex) {
                 System.out.println("Error sending error response: " + ex.toString());
             }
